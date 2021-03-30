14afb9812ad6133953eaafd7b8eab5bfc4f5905e
PC <amolujagare@gmail.com>
2021-03-30 18:06:11 +0530

    drop down

---
+++ src/LoginElements2.java
@@ -0,0 +1,33 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class LoginElements2 {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("http://stock.scriptinglogic.net/");
+
+    /*    List<WebElement> wbList  =  driver.findElements(By.tagName("input")); // 3
+
+        // how to print - how many webelements are there -- i want number as 3 in output
+        System.out.println(wbList.size());
+
+        wbList.get(0).sendKeys("admin");
+        wbList.get(1).sendKeys("admin");
+        wbList.get(2).click();
+*/
+        driver.findElements(By.tagName("input")).get(0).sendKeys("admin");
+        driver.findElements(By.tagName("input")).get(1).sendKeys("admin");
+        driver.findElements(By.tagName("input")).get(2).click();
+    }
+}
